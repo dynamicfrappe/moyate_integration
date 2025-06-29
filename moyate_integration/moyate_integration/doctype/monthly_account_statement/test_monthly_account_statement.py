d65c6efe3549252c688b78eaa00a4a1562504e65
medoo <1leomedo1@gmail.com>
2025-06-29 16:21:47 +0300

    Add Monthly Account Statement doctype and print formate

---
+++ moyate_integration/moyate_integration/doctype/monthly_account_statement/test_monthly_account_statement.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Dynamic Business Solutions and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestMonthlyAccountStatement(unittest.TestCase):
+	pass
